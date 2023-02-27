603d4dc00414f7811842b7957a82f1faf97cee4d
ASHKARAN <ali.ashkaran@gmail.com>
2023-02-27 16:30:29 +0300

    Update start.sh

--- testnet-v4/without-sentry/bor/start.sh
+++ testnet-v4/without-sentry/bor/start.sh
@@ -13,11 +13,12 @@ BOR_DIR=${BOR_DIR:-~/.bor}
 DATA_DIR=$BOR_DIR/data
 
 bor --datadir $DATA_DIR \
-  --port 30303 \
   --http --http.addr '0.0.0.0' \
   --http.vhosts '*' \
   --http.corsdomain '*' \
   --http.port 8545 \
+  --ws \
+  --ws.addr '0.0.0.0' \
   --ipcpath $DATA_DIR/bor.ipc \
   --http.api 'eth,net,web3,txpool,bor' \
   --syncmode 'full' \
@@ -36,5 +37,7 @@ bor --datadir $DATA_DIR \
   --maxpeers 50 \
   --metrics \
   --pprof --pprof.port 7071 --pprof.addr '0.0.0.0' \
-  --mine
+
+  
+  
   
