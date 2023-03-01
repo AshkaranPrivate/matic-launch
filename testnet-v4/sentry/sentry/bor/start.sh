f2ff6fcad6bda83ca7c8fa7810f33a3f2f189662
ASHKARAN <ali.ashkaran@gmail.com>
2023-03-01 11:40:03 +0300

    Update start.sh

--- testnet-v4/sentry/sentry/bor/start.sh
+++ testnet-v4/sentry/sentry/bor/start.sh
@@ -6,6 +6,7 @@ BOR_DIR=${BOR_DIR:-~/.bor}
 DATA_DIR=$BOR_DIR/data
 
 bor --datadir $DATA_DIR \
+  --snapshot false \
   --http --http.addr '0.0.0.0' \
   --ws --ws.addr '0.0.0.0' \
   --http.vhosts '0.0.0.0' \
